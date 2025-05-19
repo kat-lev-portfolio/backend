91db66998de0bed2f8913a5e574ef3dd44de2f96
kat-lev <kat@katleverton.com>
2025-05-19 09:59:37 +0200

    Create Project function in Controller

--- src/main/java/com/example/portfolio/controller/ProjectController.java
+++ src/main/java/com/example/portfolio/controller/ProjectController.java
@@ -1,1 +1,19 @@
+package com.example.portfolio.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import com.example.portfolio.model.Project;
+import com.example.portfolio.repository.ProjectRepository;
+
+@RestController
+@RequestMapping("/api/projects")
+public class ProjectController {
+    
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    @PostMapping
+    public Project createProject(@RequestBody Project project) {
+        return projectRepository.save(project);
+    }
+}
